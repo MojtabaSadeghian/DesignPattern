8002dcf2078ab2e971cb90080cfef1130510bfd9
Mojtaba Sadeghian <Mojtaba.Sadeghian@asggroup.com.au>
2020-08-16 12:02:23 +1000

    Code with the Singleton pattern.

---
+++ Singleton/Policy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    public class Policy
+    {
+        private static readonly Policy _instance = new Policy();
+
+        public static Policy Instance {
+            get
+            {
+                return _instance; 
+            } 
+        }
+        private Policy()
+        {
+
+        }
+        public int ID { get; set; } = 101;
+        public string Insured { get; set; } = "Mojtaba";
+        public string GetInsuredName() => Insured;
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main()
+        {
+            //var policy = new Policy();
+            var insuredName = Policy.Instance.GetInsuredName();
+
+            Console.WriteLine(insuredName);
+        }
+    }
+}
